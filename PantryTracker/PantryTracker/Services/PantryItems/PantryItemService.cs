eb1f80af7547a8ada113e7740175c14ee853d28c
irishandyesiam <nathan.irish@gmail.com>
2023-05-30 22:39:27 -0500

    Implementation of ErrorOr methods in PantryItemService.

--- PantryTracker/PantryTracker/Services/PantryItems/PantryItemService.cs
+++ PantryTracker/PantryTracker/Services/PantryItems/PantryItemService.cs
@@ -7,14 +7,18 @@ namespace PantryTracker.Services.PantryItems;
 public class PantryItemService : IPantryItemService
 {
     private static readonly Dictionary<Guid, PantryItem> _pantryItems = new();
-    public void CreatePantryItem(PantryItem pantryItem)
+    public ErrorOr<Created> CreatePantryItem(PantryItem pantryItem)
     {
         _pantryItems.Add(pantryItem.Id, pantryItem);
+
+        return Result.Created;
     }
 
-    public void DeleteItem(Guid id)
+    public ErrorOr<Deleted> DeleteItem(Guid id)
     {
         _pantryItems.Remove(id);
+
+        return Result.Deleted;
     }
 
     public ErrorOr<PantryItem> GetItem(Guid id)
@@ -28,8 +32,10 @@ public class PantryItemService : IPantryItemService
         
     }
 
-    public void UpsertItem(PantryItem pantryItem)
+    public ErrorOr<Updated> UpsertItem(PantryItem pantryItem)
     {
         _pantryItems[pantryItem.Id] = pantryItem;
+
+        return Result.Updated;
     }
 }
