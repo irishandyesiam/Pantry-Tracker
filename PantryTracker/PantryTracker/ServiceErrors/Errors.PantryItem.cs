dcf2f310e8056c8397a7bb5739811e87b4b80a86
irishandyesiam <nathan.irish@gmail.com>
2023-06-02 16:22:05 -0500

    Errors.PantryItems added customer code errors with descriptions.

--- PantryTracker/PantryTracker/ServiceErrors/Errors.PantryItem.cs
+++ PantryTracker/PantryTracker/ServiceErrors/Errors.PantryItem.cs
@@ -6,6 +6,16 @@ public static class Errors
 {
     public static class PantryItem
     {
+        public static Error InvalidName => Error.Validation(
+            code: "PantryItem.InvalidName",
+            description: $"PantryItem name must be at least {Models.Breakfast.MinNameLength}" +
+                $" characters long and at most {Models.PantryItem.MaxNameLength} characters long.");
+        
+        public static Error InvalidDescription => Error.Validation(
+            code: "PantryItem.InvalidDescription",
+            description: $"PantryItem description must be at least {Models.PantryItem.MinDescriptionLenght}" + 
+                $" characters long and at most {Models.PantryItem.MaxDescriptionLength} characters long.");
+
         public static Error NotFound => Error.NotFound(
             code: "PantryItem.NotFound",
             description: "Pantry item not found");
